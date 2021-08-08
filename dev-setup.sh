7c246758d01cae055a5c75031ec9562c6b0f609b
Jules Morelli <jmorelli@manas.tech>
2021-08-08 17:22:49 -0300

    Add pre-commit to the setup

--- dev-setup.sh
+++ dev-setup.sh
@@ -11,6 +11,13 @@ COLOR_GREEN="$(tput setaf 2)"
 COLOR_MAGENTA="$(tput setaf 5)"
 COLOR_LIGHT_BLUE="$(tput setaf 81)"
 
+# set up precommit hooks (this actually needs to happen outside of docker!)
+echo "$COLOR_LIGHT_BLUE 🧑‍🔧 Setting up pre-commit... $COLOR_REST"
+pip3 install --upgrade pip
+pip3 install pre-commit
+pre-commit install -f
+echo "$COLOR_LIGHT_BLUE ✨ Pre-commit is ready to rumble! $COLOR_REST"
+
 # create database
 echo "$COLOR_LIGHT_BLUE 🧑‍🔧 Ensuring DB exists... $COLOR_REST"
 docker-compose up -d db
