eddf21da12b39bb3d03f39a435372528b3b1f4b8
Jules Morelli <jmorelli@manas.tech>
2021-08-16 13:42:39 -0300

    Add tests for user registration

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+from typing import Dict, Any
 
+import requests
 import alembic.config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -33,3 +35,29 @@ def db():
     for tbl in reversed(BaseModel.metadata.sorted_tables):
         if not tbl.schema:
             engine.execute(tbl.delete())
+
+
+@pytest.fixture
+def patched_requests(monkeypatch, request):
+    method: str = request.param.get("method", "get")
+    response: Dict[str, Any] = request.param.get("response", {})
+    status_code: int = request.param.get("status_code", 200)
+
+    def mocked_method(uri: str, **kwargs):
+        """A method replacing requests[method]
+        Returns either a mocked response object (with json method)
+        or the default response object if the uri doesn't match
+        one of those that have been supplied.
+        """
+        # create a mocked requests object
+        mock = type("MockedReq", (), {})()
+        # assign mocked json to requests.json
+        mock.json = response
+        mock.status_code = status_code
+        if status_code != 200:
+            mock.error = "something went wrong!"
+        # assign obj to mock
+        return mock
+
+    # finally, patch requests.get with patched version
+    monkeypatch.setattr(requests, method, mocked_method)
--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,8 +1,19 @@
 from urllib.parse import urlencode
+import pytest
 
 from sqlalchemy.orm import Session
 
+from users.models import User
 from tests.utils import seed_user
+
+
+def _get_register_data():
+    return {
+        "email": "gothmog@angband.com",
+        "password": "supersafepw",
+        "username": "xXx_lordOfBalrogs_xXx",
+        "confirm_password": "supersafepw",
+    }
 
 
 def test_login_wrong_password(setup, db: Session):
@@ -47,6 +58,42 @@ def test_login_with_username(setup, db: Session):
         headers={"Content-Type": "application/x-www-form-urlencoded"},
     )
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "patched_requests", [{"method": "post"}], indirect=["patched_requests"]
+)
+def test_register(patched_requests, setup, db: Session):
+    data = _get_register_data()
+    response = setup.post("/api/auth/register", json=data)
+    assert response.status_code == 200
+    user = db.query(User).filter(User.email == data["email"]).one_or_none()
+    assert user is not None
+
+
+@pytest.mark.parametrize(
+    "patched_requests", [{"method": "post"}], indirect=["patched_requests"]
+)
+def test_register_with_wrong_pw(patched_requests, setup, db: Session):
+    data = _get_register_data()
+    data["confirm_password"] = f'not-the-same-{data["password"]}'
+    response = setup.post("/api/auth/register", json=data)
+    assert response.status_code == 422
+    user = db.query(User).filter(User.email == data["email"]).one_or_none()
+    assert user is None
+
+
+@pytest.mark.parametrize(
+    "patched_requests",
+    [{"method": "post", "status_code": 400}],
+    indirect=["patched_requests"],
+)
+def test_register_with_failing_email(patched_requests, setup, db: Session):
+    data = _get_register_data()
+    response = setup.post("/api/auth/register", json=data)
+    assert response.status_code == 500
+    user = db.query(User).filter(User.email == data["email"]).one_or_none()
+    assert user is not None
 
 
 def test_onboarding_no_token(setup, db: Session):
